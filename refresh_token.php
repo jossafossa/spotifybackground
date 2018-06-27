49368a100937ee40c8fec3446718a8ed86ee6b06
jossafossa <joosthobma@gmail.com>
2018-06-27 03:16:13 +0200

    progressbar + refresh token + auth to php

---
+++ refresh_token.php
@@ -0,0 +1,42 @@
+<?php 
+	
+	// api settings
+	$client_id = 'f8c1676b7739450daf60050339b297b5';
+	$client_secret = '6fa89bf4e01243d38414cee037b074e0';
+	$redirect_uri = 'http://localhost/spotifybackground/refresh_token.php';
+	$scope = [
+		"user-read-email", 
+		"user-read-currently-playing", 
+		"user-read-playback-state",
+		"user-modify-playback-state"
+	];
+	$scope = join("%20", $scope); // convert array to querystring
+
+
+
+	$token_url = "https://accounts.spotify.com/api/token";
+
+		$body = array(
+			"grant_type" => "refresh_token",
+			"refresh_token" => $_GET["refresh_token"],
+		  'client_id' => $client_id,
+		  'client_secret' => $client_secret
+		);
+
+		$opts = array(
+			'http' => array(
+		    'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'  => 'POST',
+		    'content' => http_build_query($body)
+		  )
+		);
+
+		$context  = stream_context_create($opts);
+
+		$result = json_decode( file_get_contents($token_url, false, $context) );
+
+		$token = $result->access_token;
+
+		echo json_encode($result);
+
+ ?>
--- scripts/main.js
+++ scripts/main.js
@@ -70,7 +70,36 @@ $(document).ready(function() {
 			var bg2 = $('#svg-image');
 			var title = $('.title');
 			var artist = $('.artist');
+			var timeline = $('.timeline-bar');
+			var refreshTimeout = 1000;
 
+			setInterval(function() {
+
+				$.ajax({
+					method:"GET",
+					url:"refresh_token.php",
+					data: {
+						refresh_token: _refresh_token
+					},
+					success: function(result) {
+						data = JSON.parse(result);
+						_token = data.access_token;
+						console.log("refresh_token");
+						console.log("/////////////////////////////////////////////////////////////");
+						console.log("new token = " + _token);
+						console.log("/////////////////////////////////////////////////////////////");
+
+					},
+					error: function(result) {
+						console.log("errorrrrr:");						
+						console.log(result);
+					}
+				})
+
+			}, (_expires_in - 100) * 1000)
+
+
+			
 			// set correct bg size
 			// bg2.attr("width", window.innerWidth );
 			// bg2.attr("height", window.innerHeight );
@@ -80,46 +109,97 @@ $(document).ready(function() {
 			// bg2.attr("y", -((window.innerWidth - window.innerHeight) / 2));
 
 			// Get the hash of the url
-			var hash = window.location.hash
-			.substring(1)
-			.split('&')
-			.reduce(function (initial, item) {
-			  if (item) {
-			    var parts = item.split('=');
-			    initial[parts[0]] = decodeURIComponent(parts[1]);
-			  }
-			  return initial;
-			}, {});
-			window.location.hash = '';
+			// var hash = window.location.hash
+			// .substring(1)
+			// .split('&')
+			// .reduce(function (initial, item) {
+			//   if (item) {
+			//     var parts = item.split('=');
+			//     initial[parts[0]] = decodeURIComponent(parts[1]);
+			//   }
+			//   return initial;
+			// }, {});
 
-			// Set token
-			var _token = hash.access_token;
+			// var getUrlParameter = function getUrlParameter(sParam) {
+			//     var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+			//         sURLVariables = sPageURL.split('&'),
+			//         sParameterName,
+			//         i;
 
-			var authEndpoint = 'https://accounts.spotify.com/authorize';
+			//     for (i = 0; i < sURLVariables.length; i++) {
+			//         sParameterName = sURLVariables[i].split('=');
 
-			// Replace with your app's client ID, redirect URI and desired scopes
-			var clientId = 'f8c1676b7739450daf60050339b297b5';
-			var redirectUri = 'https://slightlyshifted.nl/spotifybackground/';
-			var scopes = [
-				"user-read-email", 
-				"user-read-currently-playing", 
-				"user-read-playback-state",
-				"user-modify-playback-state"
-			];
+			//         if (sParameterName[0] === sParam) {
+			//             return sParameterName[1] === undefined ? true : sParameterName[1];
+			//         }
+			//     }
+			// };
 
-			// If there is no token, redirect to Spotify authorization
-			if (!_token) {
-			  window.location = "" + authEndpoint + "?client_id=" + clientId + "&redirect_uri=" + redirectUri + "&scope=" + scopes.join('%20') + "&response_type=token";
-			}
 
-			console.log(_token);
+
+
+			// var authEndpoint = 'https://accounts.spotify.com/authorize';
+
+			// // Replace with your app's client ID, redirect URI and desired scopes
+			// var clientId = 'f8c1676b7739450daf60050339b297b5';
+			// var clientSecret = '6fa89bf4e01243d38414cee037b074e0';
+			// var redirectUri = 'http://localhost/spotifybackground/';
+			// var scopes = [
+			// 	"user-read-email", 
+			// 	"user-read-currently-playing", 
+			// 	"user-read-playback-state",
+			// 	"user-modify-playback-state"
+			// ];
+
+
+			// // if code is not defined get a code
+			// if (getUrlParameter("code") == null ) {
+			// 	console.log("no code yet");
+			// 	window.location = "" + authEndpoint + "?client_id=" + clientId + "&redirect_uri=" + redirectUri + "&scope=" + scopes.join('%20') + "&response_type=code";
+			// } else {								
+			// 	var code = getUrlParameter("code");
+			// 	console.log("there is a code = " + code + ", getting access_token");
+			// 	console.log(JSON.stringify({
+			// 		   grant_type: "authorization_code",
+			// 		   code: code,
+			// 		   redirect_uri: redirectUri
+			// 		 }));
+			// 	$.ajax({
+			// 		method: "POST",
+			// 		contentType: "application/x-www-form-urlencoded",
+			// 	   url: "https://accounts.spotify.com/api/token",
+			// 	   headers: {
+			// 	       'Authorization': 'Basic ' + clientId + ":" + clientSecret
+			// 	   },
+			// 	   data: {
+			// 		   grant_type: "authorization_code",
+			// 		   code: code,
+			// 		   redirect_uri: redirectUri
+			// 		 },
+			// 	   success: function(response) {
+			// 	   		console.log(response)
+			// 	   }, 
+			// 	   error: function(response) {
+			// 	   	console.log("%c Error", 'color:red');
+			// 	   		console.log(response);
+			// 	   }
+			// 	});
+			// }
+
+
+
+			
+
+			// var _token = hash.access_token;
+
+			// console.log(_token);
 
 			
 			getMyCurrentTrack();
 
 			timer = setInterval(function() {
 				getMyCurrentTrack();
-			}, 1000)
+			}, refreshTimeout)
 			var playing;
 
 			function getMyCurrentTrack() {
@@ -140,12 +220,21 @@ $(document).ready(function() {
 				       	} else {				       		
 				   				$('.pause i').attr("class","fa fa-play");	
 				       	}
+				       	console.log("updating");
 				       	console.log(response);
 				       	image.css({"background-image":"url('" + cover + "')"});
 				       	bg.css({"background-image":"url('" + coverSmall + "')"});
 				       	bg2.attr({"href":coverSmall});
 				       	title.text(titleText);
 				       	artist.text(artistText);
+
+
+				       	// timeline
+
+				       	currentTime = response['progress_ms'];
+				       	duration = response['item']['duration_ms'];
+				       	progress = 100 /duration * currentTime;
+				       	timeline.css({'width': progress + '%'});
 				   }
 				});
 			};
